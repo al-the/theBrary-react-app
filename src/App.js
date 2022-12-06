204ce7fcf9824e43e9e9fc6380b1f8316619fb59
Al <107307734+al-the@users.noreply.github.com>
2022-12-06 11:44:26 +0000

    change text front page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bringing <code>React.js</code> into theBrary.
         </p>
         <a
           className="App-link"

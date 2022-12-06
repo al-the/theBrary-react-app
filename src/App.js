31715e44f2d4254c8cbc3aa8aee0dcb450d4f266
Al <107307734+al-the@users.noreply.github.com>
2022-12-07 05:24:34 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/al-the/theBrary-react-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github
         </a>
       </header>
     </div>
